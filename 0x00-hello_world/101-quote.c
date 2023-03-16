16b6273a33690378a84f80fa1caee1813a9263eb
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-16 14:55:54 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-const int length = 59;
-const int fd = 2;
+char message[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int length = 59;
+int fd = 1;
 write(fd, message, length);
 return (1);
 }
