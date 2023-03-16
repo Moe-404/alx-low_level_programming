6fa4827d52630b879adb5cdf660010976fa7cb8b
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-16 14:51:58 -0700

    0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/*
+ *hello world
+ * */
+int main(void)
+{
+const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const int length = 59;
+const int fd = 2;
+write(fd, message, length);
+return (1);
+}
