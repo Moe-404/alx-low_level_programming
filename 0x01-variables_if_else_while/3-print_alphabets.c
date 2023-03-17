cbda02b1e6e0043f504c20e909ed9980c727ff53
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-17 14:48:16 -0700

    0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char c = 'a';
+char C = 'A';
+for (i = 0; i < 26; i++)
+{
+        putchar(c++);
+}
+for (i = 0; i < 26; i++)
+{
+        putchar(C++);
+}
+putchar('\n');
+return (0);
+}
