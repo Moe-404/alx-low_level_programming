2c1e15834e523a4085962c7d2de3d9f24e9ce47a
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-17 14:53:48 -0700

    0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char c = 'a';
+for (i = 0; i < 26; i++)
+{
+if (c == 'q' || c == 'e')
+{
+c++;
+continue;
+}
+putchar(c++);
+}
+putchar('\n');
+return (0);
+}
