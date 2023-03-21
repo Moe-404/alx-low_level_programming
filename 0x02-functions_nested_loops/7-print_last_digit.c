d14f76016de2cd4ed596051862421ff9db944ce2
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 15:16:09 -0700

    0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,11 @@
 int print_last_digit(int n)
 {
 	if (n >= 0)
-		_putchar(n % 10);
-	else
+		_putchar((n % 10) + '0');
+	else(n % 10)
 	{
 		n *= -1;
-		_putchar(n % 10);
+		_putchar((n % 10) + '0');
 	}
-	return ((n % 10) + '0');
+	return (n % 10);
 }
