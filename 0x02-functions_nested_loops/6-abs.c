b43dfa08fbf1911c58af4d8a883be0b36f17e54c
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 13:06:42 -0700

    0

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ *  _abs - function returns the absloute value of an integer.
+ *
+ *  return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
