29b1130ff3428c0c924cdce3031534203faf6556
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 02:41:03 -0700

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,12 @@
-#include <stdio.h>
 #include "main.h"
-
+/**
+ * main -
+ *
+ * return: 0*/
 void print_alphabet(void)
 {
 	int i;
 	for(i = 'a'; i <= 'z'; i++)
 		putchar(i);
 	putchar('\n');
-}
-
-void print_alphabet_x10(void)
-{
-        int i;
-        int j;
-        for(i = 0; i < 10; i++)
-        {
-                for(j = 'a'; j <= 'z'; j++)
-                        putchar(j);
-                putchar('\n');
-	}
 }
