61f6064e825e0f04e8af00ecd9ed24246812907b
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 03:42:03 -0700

    0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * print_sign - functions returns 1 if num is positive or 0 if it is zero or -1 otherwise
+ * print_sign - functions returns 1 if num is positive
+ * or 0 if it is zero or -1 otherwise
+ *
  * @c: the num to be checked
  *
  * Return: 1 if num is positive or 0 if it is zero or -1 otherwise
