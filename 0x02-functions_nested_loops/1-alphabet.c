2834abb01f7caadfc1992bef921cbe174aed6e4b
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 02:56:29 -0700

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  *
  * return: 0
  */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	int i;
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
+
 	_putchar('\n');
 }
