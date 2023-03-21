3015d6b9756884c8de924ca97b29750300f91ee1
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-21 03:15:42 -0700

    0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - functions returns 1 if char is lower or 0otherwise
+ *
+ * return: 1 or 0
+ */
+
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
