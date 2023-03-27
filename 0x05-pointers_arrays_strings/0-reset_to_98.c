dfbcb2007aa7a0f5dc3bc910e9a7178950676c03
Mohamed Eid <mohamed868hu@gmail.com>
2023-03-27 08:53:02 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - function takes an int  paramerter and updates the value it points to to 98.
+ * @n: number
+ *
+ * Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: header file
+**/
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
